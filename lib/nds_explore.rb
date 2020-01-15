8f26a5fb02e214c923189a8ea929c424424c8a37
Matt Milton <mattmilton57@gmail.com>
2020-01-15 15:32:02 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-p directors_database
+  movies_by_steve = directors_database[0][:movies]
+  movies_by_steve_index = 0
+  while movies_by_steve_index < movies_by_steve.count do
+    puts movies_by_steve[movies_by_steve_index][:title]
+    movies_by_steve_index += 1 
+  end 
 end
-
+ 
